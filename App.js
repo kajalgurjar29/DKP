241bd9069cd9473961e8550fe3210f4cca6985f0
kajal gurjar <dev29.mxpertz@gmail.com>
2025-03-17 16:43:21 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import { connectDB } from "./db/db.config.js";
+import { connectDB } from "./src/db/db.config.js";
 import { createServer } from "http";
 import multer from "multer";
 dotenv.config({ path: "./.env" });
@@ -27,21 +27,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
-import userRoute from "./routers/user.router.js";
-import loginRoute from "./routers/login.router.js";
-import dkpPoolRoute from "./routers/dkppools.router.js";
-import ruleRoter from "./routers/rules.router.js";
-import eventRouter from "./routers/event.router.js";
-import actionRouter from "./routers/action.router.js";
-import profilemanagementRouter from "./routers/profileManagement.router.js";
+import userRoute from "./src/routers/user.router.js";
+import loginRoute from "./src/routers/login.router.js";
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/login", loginRoute);
-app.use("/api/v1/dkpPools", dkpPoolRoute);
-app.use("/api/v1/rules", ruleRoter);
-app.use("/api/v1/event", eventRouter);
-app.use("/api/v1/action", actionRouter);
-app.use("/api/v1/profilemanagement", profilemanagementRouter);
 
 app.use(cors());
 app.use(express.json());
